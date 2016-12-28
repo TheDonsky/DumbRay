881d65f3fc3d3d308e2b8b13989362567aef20ef
TheDonsky <nikadon.net@gmail.com>
2016-12-28 21:12:03 +0400

    Add functionality to Transform

--- Transform.h
+++ Transform.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include"Vector3.h"
+#include"Ray.h"
 
 class Transform{
 public:
@@ -8,6 +9,7 @@ public:
 	/** //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\// **/
 	/** ########################################################################## **/
 	/** Construction: **/
+	// Constructor
 	__device__ __host__ inline Transform(const Vector3 &pos = Vector3(0, 0, 0), const Vector3 &euler = Vector3(0, 0, 0), const Vector3 &scl = Vector3(1, 1, 1));
 	
 
@@ -18,8 +20,11 @@ public:
 	/** //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\// **/
 	/** ########################################################################## **/
 	/** Getters: **/
+	// Transform position
 	__device__ __host__ inline const Vector3& getPosition()const;
+	// Transform euler angles
 	__device__ __host__ inline const Vector3& getEulerAngles()const;
+	// Transform scale
 	__device__ __host__ inline const Vector3& getScale()const;
 
 
@@ -30,13 +35,19 @@ public:
 	/** //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\// **/
 	/** ########################################################################## **/
 	/** Setters: **/
+	// Sets tranform position
 	__device__ __host__ inline Transform& setPosition(const Vector3 &newPos);
+	// Moves transform (translates position)
 	__device__ __host__ inline Transform& move(const Vector3 &posDelta);
 
+	// Sets transform euler angles
 	__device__ __host__ inline Transform& setEulerAngles(const Vector3 &newEuler);
+	// Rotates transform
 	__device__ __host__ inline Transform& rotate(const Vector3 &eulerDelta);
 
+	// Sets transform scale
 	__device__ __host__ inline Transform& setScale(const Vector3 &newScl);
+	// Scales transform
 	__device__ __host__ inline Transform& upscale(const Vector3 &sclDelta);
 
 
@@ -47,10 +58,71 @@ public:
 	/** //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\// **/
 	/** ########################################################################## **/
 	/** Transform & detransform: **/
+
+	/** ========================================================== **/
+	/*| Vectors |*/
+	// Transforms Vertex
 	__device__ __host__ inline friend Vector3& operator>>=(Vector3 &v, const Transform &t);
+	// Transformed Vertex
 	__device__ __host__ inline friend Vector3 operator>>(const Vector3 &v, const Transform &t);
+	// Detransforms Vertex
 	__device__ __host__ inline friend Vector3& operator<<=(Vector3 &v, const Transform &t);
+	// Detransformed Vertex
 	__device__ __host__ inline friend Vector3 operator<<(const Vector3 &v, const Transform &t);
+	
+	/** ========================================================== **/
+	/*| Rays |*/
+	// Transforms Ray
+	__device__ __host__ inline friend Ray operator>>(const Ray &r, const Transform &trans);
+	// Transformed Ray
+	__device__ __host__ inline friend Ray& operator>>=(Ray &r, const Transform &trans);
+	// Detransforms Ray
+	__device__ __host__ inline friend Ray operator<<(const Ray &r, const Transform &trans);
+	// Detransformed Ray
+	__device__ __host__ inline friend Ray& operator<<=(Ray &r, const Transform &trans);
+
+
+
+
+
+	/** ########################################################################## **/
+	/** //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\// **/
+	/** ########################################################################## **/
+	/** Directions: **/
+
+	/** ========================================================== **/
+	/*| Vectors |*/
+	// Forward
+	__device__ __host__ inline Vector3 front()const;
+	// Back
+	__device__ __host__ inline Vector3 back()const;
+	// Right
+	__device__ __host__ inline Vector3 right()const;
+	// Left
+	__device__ __host__ inline Vector3 left()const;
+	// Up
+	__device__ __host__ inline Vector3 up()const;
+	// Down
+	__device__ __host__ inline Vector3 down()const;
+	// Direction
+	__device__ __host__ inline Vector3 direction(Vector3 dir)const;
+	
+	/** ========================================================== **/
+	/*| Rays |*/
+	// Forward
+	__device__ __host__ inline Ray frontRay()const;
+	// Back
+	__device__ __host__ inline Ray backRay()const;
+	// Right
+	__device__ __host__ inline Ray rightRay()const;
+	// Left
+	__device__ __host__ inline Ray leftRay()const;
+	// Up
+	__device__ __host__ inline Ray upRay()const;
+	// Down
+	__device__ __host__ inline Ray downRay()const;
+	// Direction
+	__device__ __host__ inline Ray ray(Vector3 dir)const;
 
 
 
--- Transform.impl.h
+++ Transform.impl.h
@@ -3,6 +3,7 @@
 
 /** -------------------------------------------------------------------------- **/
 /** Construction: **/
+// Constructor
 __device__ __host__ inline Transform::Transform(const Vector3 &pos, const Vector3 &euler, const Vector3 &scl){
 	position = pos;
 	eulerAngles = euler;
@@ -16,12 +17,15 @@ __device__ __host__ inline Transform::Transform(const Vector3 &pos, const Vector
 
 /** -------------------------------------------------------------------------- **/
 /** Getters: **/
+// Transform position
 __device__ __host__ inline const Vector3& Transform::getPosition()const{
 	return(position);
 }
+// Transform euler angles
 __device__ __host__ inline const Vector3& Transform::getEulerAngles()const{
 	return(eulerAngles);
 }
+// Transform scale
 __device__ __host__ inline const Vector3& Transform::getScale()const{
 	return(scale);
 }
@@ -32,31 +36,37 @@ __device__ __host__ inline const Vector3& Transform::getScale()const{
 
 /** -------------------------------------------------------------------------- **/
 /** Setters: **/
+// Sets tranform position
 __device__ __host__ inline Transform& Transform::setPosition(const Vector3 &newPos){
 	position = newPos;
 	return(*this);
 }
+// Moves transform (translates position)
 __device__ __host__ inline Transform& Transform::move(const Vector3 &posDelta){
 	position += posDelta;
 	return(*this);
 }
 
+// Sets transform euler angles
 __device__ __host__ inline Transform& Transform::setEulerAngles(const Vector3 &newEuler){
 	eulerAngles = newEuler;
 	rebake();
 	return(*this);
 }
+// Rotates transform
 __device__ __host__ inline Transform& Transform::rotate(const Vector3 &eulerDelta){
 	eulerAngles += eulerDelta;
 	rebake();
 	return(*this);
 }
 
+// Sets transform scale
 __device__ __host__ inline Transform& Transform::setScale(const Vector3 &newScl){
 	scale = newScl;
 	rebake();
 	return(*this);
 }
+// Scales transform
 __device__ __host__ inline Transform& Transform::upscale(const Vector3 &sclDelta){
 	scale += sclDelta;
 	rebake();
@@ -67,22 +77,125 @@ __device__ __host__ inline Transform& Transform::upscale(const Vector3 &sclDelta
 
 
 
-/** -------------------------------------------------------------------------- **/
+/** ########################################################################## **/
+/** //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\// **/
+/** ########################################################################## **/
 /** Transform & detransform: **/
+
+/** ========================================================== **/
+/*| Vectors |*/
+// Transforms Vertex
 __device__ __host__ inline Vector3& operator>>=(Vector3 &v, const Transform &t){
 	return(v(v*t.trans.x + t.position.x, v*t.trans.y + t.position.y, v*t.trans.z + t.position.z));
 }
+// Transformed Vertex
 __device__ __host__ inline Vector3 operator>>(const Vector3 &v, const Transform &t){
 	return(Vector3(v*t.trans.x + t.position.x, v*t.trans.y + t.position.y, v*t.trans.z + t.position.z));
 }
 
+// Detransforms Vertex
 __device__ __host__ inline Vector3& operator<<=(Vector3 &v, const Transform &t){
 	Vector3 delta = v - t.position;
 	return(v(delta*t.detrans.x, delta*t.detrans.y, delta*t.detrans.z));
 }
+// Detransformed Vertex
 __device__ __host__ inline Vector3 operator<<(const Vector3 &v, const Transform &t){
 	Vector3 delta = v - t.position;
 	return(Vector3(delta*t.detrans.x, delta*t.detrans.y, delta*t.detrans.z));
+}
+
+/** ========================================================== **/
+/*| Rays |*/
+// Transforms Ray
+__device__ __host__ inline Ray operator>>(const Ray &r, const Transform &trans) {
+	return Ray(r.origin >> trans, trans.direction(r.direction));
+}
+// Transformed Ray
+__device__ __host__ inline Ray& operator>>=(Ray &r, const Transform &trans) {
+	r.origin >>= trans;
+	r.direction = trans.direction(r.direction);
+	return r;
+}
+// Detransforms Ray
+__device__ __host__ inline Ray operator<<(const Ray &r, const Transform &trans) {
+	return Ray(r.origin << trans, Vector3(r.direction * trans.detrans.x, r.direction * trans.detrans.y, r.direction * trans.detrans.z));
+}
+// Detransformed Ray
+__device__ __host__ inline Ray& operator<<=(Ray &r, const Transform &trans) {
+	r.origin <<= trans;
+	r.direction(r.direction * trans.detrans.x, r.direction * trans.detrans.y, r.direction * trans.detrans.z);
+	return r;
+}
+
+
+
+
+
+/** ########################################################################## **/
+/** //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\// **/
+/** ########################################################################## **/
+/** Directions: **/
+
+/** ========================================================== **/
+/*| Vectors |*/
+// Forward
+__device__ __host__ inline Vector3 Transform::front()const {
+	return (trans.z / scale);
+}
+// Back
+__device__ __host__ inline Vector3 Transform::back()const {
+	return (-trans.z / scale);
+}
+// Right
+__device__ __host__ inline Vector3 Transform::right()const {
+	return (trans.x / scale);
+}
+// Left
+__device__ __host__ inline Vector3 Transform::left()const {
+	return (-trans.x / scale);
+}
+// Up
+__device__ __host__ inline Vector3 Transform::up()const {
+	return (trans.y / scale);
+}
+// Down
+__device__ __host__ inline Vector3 Transform::down()const {
+	return (-trans.y / scale);
+}
+// Direction
+__device__ __host__ inline Vector3 Transform::direction(Vector3 dir)const {
+	return ((dir.x * (trans.x / scale)) + (dir.y * (trans.y / scale)) + (dir.z * (trans.z / scale)));
+}
+
+/** ========================================================== **/
+/*| Rays |*/
+// Forward
+__device__ __host__ inline Ray Transform::frontRay()const {
+	return Ray(position, front());
+}
+// Back
+__device__ __host__ inline Ray Transform::backRay()const {
+	return Ray(position, back());
+}
+// Right
+__device__ __host__ inline Ray Transform::rightRay()const {
+	return Ray(position, right());
+}
+// Left
+__device__ __host__ inline Ray Transform::leftRay()const {
+	return Ray(position, left());
+}
+// Up
+__device__ __host__ inline Ray Transform::upRay()const {
+	return Ray(position, up());
+}
+// Down
+__device__ __host__ inline Ray Transform::downRay()const {
+	return Ray(position, down());
+}
+// Direction
+__device__ __host__ inline Ray Transform::ray(Vector3 dir)const {
+	return Ray(position, direction(dir));
 }
 
 
