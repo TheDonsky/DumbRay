f600733785c3e5db903a5c5870eae30a130d0e29
TheDonsky <nikadon.net@gmail.com>
2017-03-20 18:23:12 +0400

    Refactor code

--- Pair.cuh
+++ Pair.cuh
@@ -2,24 +2,25 @@
 
 #include"TypeTools.cuh"
 
+template<typename Type1, typename Type2> struct Pair;
+TYPE_TOOLS_REDEFINE_2_PART_TEMPLATE(Pair, Type1, Type2, typename Type1, typename Type2);
+
+
 template<typename Type1, typename Type2>
-struct Pair;
-template<typename Type1, typename Type2>
-class TypeTools<Pair<Type1, Type2> >{
+class Pair{
 public:
-	typedef Pair<Type1, Type2> ElementType;
-	DEFINE_TYPE_TOOLS_CONTENT_FOR(ElementType);
-};
-
-
-
-template<typename Type1, typename Type2>
-struct Pair{
 	Type1 first;
 	Type2 second;
 
 	__device__ __host__ inline Pair();
 	__device__ __host__ inline Pair(const Type1 &f, const Type2 &s);
+
+
+
+
+
+private:
+	TYPE_TOOLS_ADD_COMPONENT_GETTERS_2(Pair, first, second);
 };
 
 
