e8899d4697f386f74baa9dc1c9dafef72cd44f4a
TheDonsky <nikadon.net@gmail.com>
2017-10-15 17:46:29 +0400

    Make Test module more usable

--- __source__/Test.cu
+++ __source__/Test.cu
@@ -1,200 +1,86 @@
 #include"DataStructures/GeneralPurpose/Stacktor/Stacktor.test.cuh"
-#include"DataStructures/Objects/Meshes/PolyMesh/PolyMesh.h"
-#include"Namespaces/MeshReader/MeshReader.h"
 #include"DataStructures/GeneralPurpose/IntMap/IntMap.test.h"
-#include"Namespaces/Windows/Windows.h"
 #include"DataStructures/GeneralPurpose/Cutex/Cutex.test.cuh"
 #include"DataStructures/Objects/Scene/Raycasters/Octree/Octree.test.cuh"
-#include"DataStructures/Primitives/Pure/ColorRGB/ColorRGB.cuh"
-#include"DataStructures/Objects/Components/Shaders/DummyShader/DummyShader.cuh"
-#include"DataStructures/Primitives/Pure/Vector2/Vector2.h"
 #include"DataStructures/GeneralPurpose/Generic/Generic.test.cuh"
 #include"DataStructures/Objects/Components/Lenses/Lense.test.cuh"
 #include"DataStructures/Renderers/BackwardTracer/BackwardTracer.test.cuh"
 #include"DataStructures/GeneralPurpose/Handler/Handler.test.cuh"
 #include"DataStructures/Objects/Components/Shaders/Material.test.cuh"
 #include"DataStructures/GeneralPurpose/TypeTools/TypeTools.test.cuh"
-#include"DataStructures/Objects/Scene/Scene.cuh"
-#include"DataStructures/Primitives/Compound/Pair/Pair.cuh"
-#include"Namespaces/Device/Device.cuh"
 #include"DataStructures/Objects/Scene/SceneHandler/SceneHandler.test.cuh"
 #include"DataStructures/Renderers/Renderer/Renderer.test.cuh"
-
-
-inline static __device__ __host__ Color color(int y, int x, float time){
-	//*
-	float i = ((float)y) / 4.0f;
-	float j = ((float)x) / 4.0f;
-	time /= 16.0f;
-	Color c;
-	c.r = (sin(i * j + time) + 1) / 2.0f;
-	c.g = (cos(i / j - time) + 1) / 2.0f;
-	c.b = ((int)tan(i + j + time) % 256) / 256.0f;
-	c.r = sin(PI * c.r + PI * c.g + PI * c.b);
-	c.g = cos(PI * c.r + PI * c.g + PI * c.b);
-	c.b = sin(PI * c.r + PI * c.g + PI * c.b) * cos(PI * c.r + PI * c.g + PI * c.b);
-	return c;
-}
-
-inline static void color(Matrix<Color> &mat, int time){
-	time /= 16;
-	for (int i = 0; i < mat.height(); i++)
-		for (int j = 0; j < mat.width(); j++)
-			mat[i][j] = color(i, j, (float)time);
-}
-
-
-#define UNITS_PER_THREAD 8
-#define UNITS_PER_BLOCK 4
-__global__ static void color(Color *data, int width, int height, int time){
-	int y = blockIdx.x * UNITS_PER_BLOCK;
-	int startX = threadIdx.x * UNITS_PER_THREAD;
-	int endY = y + UNITS_PER_BLOCK; if (endY > height) endY = height;
-	int endX = startX + UNITS_PER_THREAD; if (endX > width) endX = width;
-	while (y < endY){
-		for (int x = startX; x < endX; x++)
-			data[y * width + x] = color(y, x, time);
-		y++;
-	}
-}
-
-static int numBlocks(int height){
-	return ((height + UNITS_PER_BLOCK - 1) / UNITS_PER_BLOCK);
-}
-static int numThreads(int width){
-	return ((width + UNITS_PER_THREAD - 1) / UNITS_PER_THREAD);
-}
-
-static bool colorPixels(Color *data, int width, int height, int time){
-	time /= 16;
-	cudaStream_t stream; if (cudaStreamCreate(&stream) != cudaSuccess) return false;
-	color<<<numBlocks(height), numThreads(width), 0, stream>>>(data, width, height, time);
-	bool success = (cudaStreamSynchronize(stream) == cudaSuccess);
-	if (cudaStreamDestroy(stream) != cudaSuccess) success = false;
-	return success;
-}
-
-inline static void testWindow(){
-	Windows::Window w;
-	int i = 0;
-	Matrix<Color> mat;
-	long time = clock();
-	long consumedTime = 0;
-	long lastTime = clock();
-	
-	Color *image = NULL;
-	int imgWidth = 0;
-	int imgHeight = 0;
-
-	bool device = false;
-	short oldState = false;
-	while (true){
-		if (w.dead()) break;
-
-		int width, height;
-		w.getDimensions(width, height);
-		if (!device){
-			if (mat.width() != width || mat.height() != height){
-				mat.setDimensions(width, height);
-				continue;
+#include"Namespaces/Device/Device.cuh"
+#include <map>
+#include <string>
+#include <ctype.h>
+namespace {
+	typedef void(*TestFunction)();
+	struct TestEntry {
+		std::string description;
+		TestFunction function;
+	};
+	static void test() {
+		cudaSetDevice(0);
+		Device::dumpCurrentDevice();
+		std::map<std::string, TestEntry> tests;
+		tests["scene_handler"] = { "Basic test for SceneHandler structure", SceneHandlerTest::test };
+		tests["renderer"] = { "Basic test for standrad Renderer pipeline", RendererTest::test };
+		tests["type_tools"] = { "General tests for TypeTools and it's default implementations", TypeToolsTest::test };
+		tests["backward_tracer"] = { std::string("Test for BackwardTracer\n") 
+			+ "    (legacy; can and most likely, will cause freeze and/or a crash)", BackwardTracerTest::test };
+		tests["handler"] = { "General test for generic Handler type", HandlerTest::test };
+		tests["generic"] = { "General test for Generic interface", GenericTest::test };
+		tests["lense_test_memory"] = { "Simple test for Lense", LenseTest::testMemory };
+		tests["material"] = { "Simple test for material", MaterialTest::test };
+		tests["octree"] = { std::string("Combined (CPU/GPU) performance test for Octree\n")
+			+ "    (simulates rendering with no bounces and shadows;\n"
+			+ "    swaps out device when space is pressed)", OctreeTest::test };
+		tests["octree_continuous"] = { "Runs 'octree' test over and over again", OctreeTest::runtContinuousTest };
+		tests["cutex"] = { std::string("Basic functionality test for Cutex\n")
+			+ "    (cuda-friendly mutex equivalent)", CutexTest::test };
+		tests["stacktor_local"] = { "Tests for Stacktor local functionality (no upload/destroy)", StacktorTest::localTest };
+		tests["stacktor_load"] = { "Tests for Stacktor upload/destroy functionality", StacktorTest::loadTest };
+		tests["stacktor_full"] = { "Full Stacktor test, combining both load and local", StacktorTest::test };
+		tests["stacktor"] = { "Short for 'stacktor_full'", StacktorTest::test };
+		tests["int_map"] = { "Basic tests for IntMap", IntMapTest::test };
+		std::cout << "___________________________________________________________________" << std::endl;
+		std::cout << "WELCOME TO DumbRay TESTING MODULE" << std::endl << "(enter ? for further instructions or any test to run)" << std::endl;
+		while (true) {
+			std::cout << "--> ";
+			std::string line;
+			std::getline(std::cin, line);
+			std::string command;
+			size_t i = 0;
+			while (i < line.length() && isspace(line[i])) i++;
+			while (i < line.length() && (!isspace(line[i]))) {
+				command += tolower(line[i]);
+				i++;
 			}
-			color(mat, consumedTime);
-			w.updateFrameHost(mat);
-		}
-		else{
-			if (imgWidth != width || imgHeight != height || image == NULL){
-				if (image != NULL) cudaFree(image);
-				if (cudaMalloc(&image, sizeof(Color) * width * height) != cudaSuccess){
-					image = NULL;
-					std::cout << "ERROR" << std::endl;
-					continue;
+			if (command == "?") {
+				std::cout << "___________________________________________________________________" << std::endl;
+				std::cout << "AVAILABLE TESTS: " << std::endl;
+				for (std::map<std::string, TestEntry>::const_iterator iterator = tests.begin(); iterator != tests.end(); iterator++) {
+					std::cout << "____________________________" << std::endl;
+					std::cout << iterator->first << ":\n    " << iterator->second.description << std::endl;
 				}
-				imgWidth = width;
-				imgHeight = height;
+				std::cout << "___________________________________________________________________" << std::endl;
+				std::cout << "SPECIAL COMMANDS: " << std::endl;
+				std::cout << "____________________________" << std::endl;
+				std::cout << "?:\n    List available commands" << std::endl;
+				std::cout << "____________________________" << std::endl;
+				std::cout << "exit:\n    Exit testing module" << std::endl;
+				std::cout << std::endl << std::endl;
 			}
-			if (!colorPixels(image, imgWidth, imgHeight, consumedTime))
-				std::cout << "ERROR COLORING" << std::endl;
-			w.updateFrameDevice(image, imgWidth, imgHeight);
+			else if (command == "exit") break;
+			else {
+				std::map<std::string, TestEntry>::const_iterator iterator = tests.find(command);
+				if (iterator != tests.end()) iterator->second.function();
+			}
 		}
-
-		short state = GetKeyState(VK_SPACE);
-		if (state && oldState != state){
-			std::cout << "Changing state...";
-			device = !device;
-			if (device) std::cout << "USING DEVICE" << std::endl;
-			else std::cout << "USING HOST" << std::endl;
-		}
-		oldState = state;
-
-		int deltaTime = (int)(clock() - lastTime);
-		lastTime = clock();
-		consumedTime += deltaTime;
-		if (i % 100 == 0){
-			std::cout << "FPS: " << (100.0f * (float)CLOCKS_PER_SEC / (float)(clock() - time)) << std::endl;
-			time = clock();
-		}
-		i++;
+		cudaDeviceReset();
 	}
 }
-
-static void makeAndDestroyWindow(){
-	Windows::Window window("I Will Close");
-}
-static void windowTrap(){
-	Windows::Window window("CLOSE ME IF YOU CAN");
-	while (!window.dead()) Sleep(256);
-	std::thread trap1(windowTrap);
-	std::thread trap2(windowTrap);
-	trap1.join();
-	trap2.join();
-}
-
-__dumb__ void printVector(const char *label, const Vector3 &v){
-	printf("%s: (%f, %f, %f)\n", label, v.x, v.y, v.z);
-}
-
-__global__ void testConstantsKernel(){
-	if (blockIdx.x == 0 && threadIdx.x == 0){
-		printVector(" zero", Vector3::zero());
-		printVector("  one", Vector3::one());
-		printVector("   up", Vector3::up());
-		printVector(" down", Vector3::down());
-		printVector("front", Vector3::front());
-		printVector(" back", Vector3::back());
-		printVector("right", Vector3::right());
-		printVector(" left", Vector3::left());
-		printVector("Xaxis", Vector3::Xaxis());
-		printVector("Yaxis", Vector3::Yaxis());
-		printVector("Zaxis", Vector3::Zaxis());
-		printVector("    i", Vector3::i());
-		printVector("    j", Vector3::j());
-		printVector("    k", Vector3::k());
-		printf("\n");
-	}
-}
-static void testConstants() {
-	testConstantsKernel<<<1, 1>>>();
-	cudaDeviceSynchronize();
-}
-
 int main(){
-	cudaSetDevice(0);
-	Device::dumpCurrentDevice();
-	SceneHandlerTest::test();
-	//RendererTest::test();
-	//TypeToolsTest::test();
-	//BackwardTracerTest::test();
-	//HandlerTest::test();
-	//GenericTest::test();
-	//LenseTest::testMemory();
-	testConstants();
-	MaterialTest::test();
-	OctreeTest::runtContinuousTest();
-	CutexTest::test();
-	StacktorTest::Load::test();
-	StacktorTest::fullTest();
-	IntMapTest::test();
-	makeAndDestroyWindow();
-	testWindow();
-	cudaDeviceReset();
+	test();
 }
