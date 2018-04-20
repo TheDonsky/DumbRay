659b897909655704b29d6050c078778db93d5415
TheDonsky <nikadon.net@gmail.com>
2018-04-20 13:55:46 +0400

    Expand the interface

--- __source__/DataStructures/Renderers/BufferedRenderProcess/BufferedRenderProcess.cu
+++ __source__/DataStructures/Renderers/BufferedRenderProcess/BufferedRenderProcess.cu
@@ -6,25 +6,32 @@
 #define FLAG_DOUBLE_BUFFERED_RENDER_MODE		2
 
 #define FLAG(flag) (((uint16_t)1) << flag)
-#define HAS_FLAG(flag)			((flags & FLAG(flag)) != 0)
-#define SET_FLAG(flag, value)	flags = (value ? (flags | FLAG(flag)) : (flags & (~FLAG(flag))))
+#define HAS_FLAG_ALL(all, flag)			((all & FLAG(flag)) != 0)
+#define SET_FLAG_ALL(all, flag, value)	all = (value ? (all | FLAG(flag)) : (all & (~FLAG(flag))))
+#define HAS_FLAG(flag) HAS_FLAG_ALL(flags, flag)
+#define SET_FLAG(flag, value) SET_FLAG_ALL(flags, flag, value)
 
 
 BufferedRenderProcess::BufferedRenderProcess() {
 	flags = 0;
+	targetRenderer = NULL;
+	front = back = NULL;
+	iterations = -1;
+	bufferedWindow = NULL;
+	targetWidth = targetHeight = -1;
 }
 BufferedRenderProcess::~BufferedRenderProcess() { end(); }
 
 
 void BufferedRenderProcess::start() {
-	std::lock_guard<std::mutex> guard(settingsLock);
+	std::lock_guard<std::mutex> guard(threadLock);
 	if (HAS_FLAG(FLAG_RENDER_PROCESS_THREAD_STARTED)) return;
 	SET_FLAG(FLAG_RENDER_PROCESS_THREAD_KILL_ISSUED, false);
 	SET_FLAG(FLAG_RENDER_PROCESS_THREAD_STARTED, true);
 	renderThread = std::thread(renderProcess, this);
 }
 void BufferedRenderProcess::end() {
-	std::lock_guard<std::mutex> guard(settingsLock);
+	std::lock_guard<std::mutex> guard(threadLock);
 	if (!HAS_FLAG(FLAG_RENDER_PROCESS_THREAD_STARTED)) return;
 	SET_FLAG(FLAG_RENDER_PROCESS_THREAD_KILL_ISSUED, true);
 	renderThread.join();
@@ -32,7 +39,8 @@ void BufferedRenderProcess::end() {
 }
 
 void BufferedRenderProcess::synchSettings() {
-	std::unique_lock<std::mutex> lock(settingsLock);
+	std::unique_lock<std::mutex> lock(threadLock);
+	if (!HAS_FLAG(FLAG_RENDER_PROCESS_THREAD_STARTED)) return;
 	synchCond.wait(lock);
 }
 
@@ -41,17 +49,28 @@ void BufferedRenderProcess::synchSettings() {
 
 
 void BufferedRenderProcess::renderProcess(BufferedRenderProcess *target) {
-	target->renderProcessThread();
-}
-void BufferedRenderProcess::renderProcessThread() {
-	while(!HAS_FLAG(FLAG_RENDER_PROCESS_THREAD_KILL_ISSUED)) {
+	while (true) {
 		{
-			std::lock_guard<std::mutex> guard(settingsLock);
-			// EXTRACT CURRENT SETTING HERE...
+			std::lock_guard<std::mutex> guard(target->threadLock);
+			if (HAS_FLAG_ALL(target->flags, FLAG_RENDER_PROCESS_THREAD_KILL_ISSUED)) break;
+		}
+		BufferedRenderer *renderer;
+		FrameBufferManager *frontBuffer, *backBuffer;
+		int targetIterations;
+		BufferedWindow *bufferedWindow;
+		int targetWidth, targetHaight;
+		{
+			std::lock_guard<std::mutex> guard(target->settingsLock);
+			renderer = target->targetRenderer;
+			frontBuffer = target->front;
+			backBuffer = target->back;
+			targetIterations = target->iterations;
+			targetWidth = target->targetWidth;
+			targetHaight = target->targetHeight;
 		}
 		{
 			// RENDER SINGLE ITERATION HERE...
 		}
-		synchCond.notify_all();
+		target->synchCond.notify_all();
 	}
 }
--- __source__/DataStructures/Renderers/BufferedRenderProcess/BufferedRenderProcess.cuh
+++ __source__/DataStructures/Renderers/BufferedRenderProcess/BufferedRenderProcess.cuh
@@ -16,16 +16,31 @@ public:
 	void start();
 	void end();
 
+	void setRenderer(BufferedRenderer *renderer);
+	void setBuffer(FrameBufferManager *buffer);
+	void setDoubleBuffers(FrameBufferManager *a, FrameBufferManager *b);
+	void setTargetIterations(int targetIterations);
+	void setInfinateTargetIterations();
+	void setTargetDisplayWindows(BufferedWindow *window);
+	void setTargetResolution(int width, int height);
+	void setTargetResolutionToWindowSize();
+
 	void synchSettings();
 
 
 private:
 	volatile uint16_t flags;
+	std::mutex threadLock;
 	std::mutex settingsLock;
 	std::thread renderThread;
 	std::condition_variable synchCond;
 
+	BufferedRenderer *targetRenderer;
+	FrameBufferManager *front, *back;
+	int iterations;
+	BufferedWindow *bufferedWindow;
+	int targetWidth, targetHeight;
+
 	static void renderProcess(BufferedRenderProcess *target);
-	void renderProcessThread();
 };
 
