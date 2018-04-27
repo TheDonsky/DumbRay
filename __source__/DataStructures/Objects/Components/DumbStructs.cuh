52d08fc808e00406a5a82f7c9648cbb645572ca7
TheDonsky <nikadon.net@gmail.com>
2018-04-27 14:51:41 +0400

    Add a proper sampling structures to the equasion

--- __source__/DataStructures/Objects/Components/DumbStructs.cuh
+++ __source__/DataStructures/Objects/Components/DumbStructs.cuh
@@ -1,5 +1,42 @@
+#pragma once
 #include"../../Primitives/Compound/Photon/Photon.cuh"
 #include"../../GeneralPurpose/Stacktor/Stacktor.cuh"
 
 
 typedef Stacktor<Photon, 16> PhotonPack;
+
+template<typename Type>
+struct DumbSamples {
+	Type samples[16];
+	int sampleCount;
+
+	__device__ __host__ inline void add(const Type &sample) {
+		samples[sampleCount] = sample;
+		sampleCount++;
+	}
+
+	template<typename... Rest>
+	__device__ __host__ inline void add(const Type &sample, const Rest&... rest) {
+		add(sample);
+		add(rest...);
+	}
+
+	template<typename... Elems>
+	__device__ __host__ inline void set(const Elems&... elems) {
+		sampleCount = 0;
+		add(elems...);
+	}
+};
+
+struct SampleRay {
+	Ray ray;
+	float sampleWeight;
+
+	__device__ __host__ inline SampleRay() {}
+	__device__ __host__ inline SampleRay(Ray sample, float mass) {
+		ray = sample; sampleWeight = mass;
+	}
+};
+
+typedef DumbSamples<Photon> PhotonSamples;
+typedef DumbSamples<SampleRay> RaySamples;
