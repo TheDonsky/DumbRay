473ea0981d1372ba102dcb0df37e9a10b11dabd8
TheDonsky <nikadon.net@gmail.com>
2018-05-17 17:18:00 +0400

    Modify the shader a bit..

--- __source__/DataStructures/Objects/Components/Shaders/DumbBasedShader/DumbBasedShader.cuh
+++ __source__/DataStructures/Objects/Components/Shaders/DumbBasedShader/DumbBasedShader.cuh
@@ -6,7 +6,19 @@
 
 class DumbBasedShader {
 public:
-	__dumb__ DumbBasedShader(const ColorRGB &fresnelFactor = ColorRGB(1.022f, 0.782f, 0.344f), float cpecular = 16.0f);
+	/*
+	__dumb__ DumbBasedShader(
+		const ColorRGB &fresnelFactor = ColorRGB(1.022f, 0.782f, 0.344f), 
+		float cpecular = 16.0f, 
+		const ColorRGB &diffuse = ColorRGB(0.0f, 0.0f, 0.0f), 
+		float metal = 1.0f);
+	/*/
+	__dumb__ DumbBasedShader(
+		const ColorRGB &fresnelFactor = ColorRGB(0.175f, 0.15f, 0.125f), 
+		float cpecular = 4096.0f, 
+		const ColorRGB &diffuse = ColorRGB(0.32f, 0.32f, 0.32f), 
+		float metal = 0.25f);
+	//*/
 
 	__dumb__ void requestIndirectSamples(const ShaderInirectSamplesRequest<BakedTriFace> &request, RaySamples *samples)const;
 	__dumb__ Color getReflectedColor(const ShaderReflectedColorRequest<BakedTriFace> &request)const;
@@ -15,7 +27,9 @@ public:
 
 private:
 	ColorRGB fres;
+	ColorRGB diff;
 	float spec;
+	float specMass;
 
 	__dumb__ static float fresnel(float r, const Vector3 &wh, const Vector3 &wi);
 };
--- __source__/DataStructures/Objects/Components/Shaders/DumbBasedShader/DumbBasedShader.impl.cuh
+++ __source__/DataStructures/Objects/Components/Shaders/DumbBasedShader/DumbBasedShader.impl.cuh
@@ -2,9 +2,12 @@
 
 
 
-__dumb__ DumbBasedShader::DumbBasedShader(const ColorRGB &fresnelFactor, float cpecular) {
+__dumb__ DumbBasedShader::DumbBasedShader(const ColorRGB &fresnelFactor, float cpecular, const ColorRGB &diffuse, float metal) {
 	fres = fresnelFactor;
+	metal = max(min(metal, 1.0f), 0.0f);
+	diff = diffuse * (1.0f - metal);
 	spec = cpecular;
+	specMass = metal;
 }
 
 
@@ -15,6 +18,9 @@ __dumb__ void DumbBasedShader::requestIndirectSamples(const ShaderInirectSamples
 	if ((n * (request.ray.direction)) >= 0.0f) return;
 	Vector3 r = (request.ray.direction).reflection(n).normalized();
 	if (r * normal < 0) normal *= -1;
+	
+	//Vector3 diffuseSample; request.context->entropy->pointOnSphere(diffuseSample.x, diffuseSample.y, diffuseSample.z);
+	//if ((diffuseSample * normal) < 0.0f) diffuseSample *= -1;
 
 	Vector3 a = r & Vector3(1.0f, 0.0f, 0.0f);
 	if (normal.sqrMagnitude() < VECTOR_EPSILON) a = r & Vector3(0.0f, 1.0f, 0.0f);
@@ -29,6 +35,8 @@ __dumb__ void DumbBasedShader::requestIndirectSamples(const ShaderInirectSamples
 		sampleDir = ((r * cosine) + (sinus * ((angleCos * a) + (angleSin * b))));
 		if ((sampleDir * normal) >= 0.0f) break;
 	}
+
+	//samples->set(SampleRay(Ray(request.hitPoint, ((specMass * sampleDir) + ((1.0f - specMass) * diffuseSample))), 1.0f));
 	samples->set(SampleRay(Ray(request.hitPoint, sampleDir), 1.0f));
 }
 __dumb__ Color DumbBasedShader::getReflectedColor(const ShaderReflectedColorRequest<BakedTriFace> &request)const {
@@ -39,17 +47,27 @@ __dumb__ Color DumbBasedShader::getReflectedColor(const ShaderReflectedColorRequ
 	Vector3 w0 = request.observerDirection.normalized();
 	Vector3 wh = (w0 + wi).normalized();
 	Color fwi = Color(fresnel(fres.r, wh, wi), fresnel(fres.g, wh, wi), fresnel(fres.b, wh, wi));
-	
+
 	float dwh = ((spec + 2) / (2.0f * PI));
 	if (request.photonType == PHOTON_TYPE_DIRECT_ILLUMINATION)
 		dwh *= pow(n * wh, spec);
-	
+
 	float gwiw0 = min(1.0f, min(2.0f * (n * wh) * (n * w0) / (w0 * wh), 2.0f * (n * wh) * (n * wi) / (w0 * wh)));
-	
+
 	Color brdfBare = ((fwi * dwh * gwiw0) / (4.0f * (n * w0) * (n * wi)));
-	Color brdf(max(0.0f, min(brdfBare.r, 1.0f)), max(0.0f, min(brdfBare.g, 1.0f)), max(0.0f, min(brdfBare.b, 1.0f)));
 	
-	return brdf * request.photon.color;
+	Color brdf(
+		max(0.0f, min(brdfBare.r, 1.0f)),
+		max(0.0f, min(brdfBare.g, 1.0f)),
+		max(0.0f, min(brdfBare.b, 1.0f)));
+
+	Color diffuseColor = (diff * (n * wi));
+	Color color(
+		max(0.0f, min((brdf.r * specMass) + diffuseColor.r, 1.0f)),
+		max(0.0f, min((brdf.g * specMass) + diffuseColor.g, 1.0f)),
+		max(0.0f, min((brdf.b * specMass) + diffuseColor.b, 1.0f)));
+
+	return (color * request.photon.color);
 }
 
 
